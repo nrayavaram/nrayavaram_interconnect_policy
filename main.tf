b7dc25e77cc1f4526f9c52a41f3c5f83533f59e5
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-25 15:19:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,30 @@ resource "google_compute_router" "foobar" {
   }
 }
 
+resource "google_compute_network" "foobar" {
+  name                    = "on-dev-appid-sys-bkonprem-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_interconnect_attachment" "ipsec-encrypted-interconnect-attachment" {
+  name                     = "on-dev-appid-sys-bkonprem-attachment"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+  type                     = "PARTNER"
+  router                   = google_compute_router.router.id
+  encryption               = "IPSEC"
+  ipsec_internal_addresses = [
+    google_compute_address.address.self_link,
+  ]
+}
+
+resource "google_compute_address" "address" {
+  name          = "on-dev-appid-sys-test-address"
+  address_type  = "INTERNAL"
+  purpose       = "IPSEC_INTERCONNECT"
+  address       = "192.168.1.0"
+  prefix_length = 29
+  network       = google_compute_network.network.self_link
+}
 
 resource "google_compute_router" "router" {
   name                          = "on-dev-appid-sys-test-router"
@@ -30,3 +54,9 @@ resource "google_compute_router" "router" {
     asn = 16550
   }
 }
+
+resource "google_compute_network" "network" {
+  name                    = "on-dev-appid-sys-test-network"
+  auto_create_subnetworks = false
+}
+
