1fb39fd1f47abccc6ce985394341934b92ff4f06
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-25 15:28:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 
 resource "google_compute_interconnect_attachment" "on_prem" {
-  name                     = "on-dev-appid-sys-bkonprem-icvlan"
+  name                     = "on-dev-appid-syst-bkonprem-icvlan"
   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
   type                     = "PARTNER"
   router                   = google_compute_router.foobar.id
@@ -14,7 +14,7 @@ resource "google_compute_interconnect_attachment" "on_prem" {
 }
 
 resource "google_compute_router" "foobar" {
-  name    = "on-dev-appid-sys-bkonprem-router"
+  name    = "on-dev-appid-syst-bkonprem-router"
   network = google_compute_network.foobar.name
   bgp {
     asn = 16550
@@ -22,12 +22,12 @@ resource "google_compute_router" "foobar" {
 }
 
 resource "google_compute_network" "foobar" {
-  name                    = "on-dev-appid-sys-bkonprem-network"
+  name                    = "on-dev-appid-syst-bkonprem-network"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_interconnect_attachment" "ipsec-encrypted-interconnect-attachment" {
-  name                     = "on-dev-appid-sys-bkonprem-attachment"
+  name                     = "on-dev-appid-syst-bkonprem-attachment"
   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
   type                     = "PARTNER"
   router                   = google_compute_router.router.id
@@ -38,7 +38,7 @@ resource "google_compute_interconnect_attachment" "ipsec-encrypted-interconnect-
 }
 
 resource "google_compute_address" "address" {
-  name          = "on-dev-appid-sys-bktest-address"
+  name          = "on-dev-appid-syst-bktest-address"
   address_type  = "INTERNAL"
   purpose       = "IPSEC_INTERCONNECT"
   address       = "192.168.1.0"
@@ -47,7 +47,7 @@ resource "google_compute_address" "address" {
 }
 
 resource "google_compute_router" "router" {
-  name                          = "on-dev-appid-sys-bktest-router"
+  name                          = "on-dev-appid-syst-bktest-router"
   network                       = google_compute_network.network.name
   encrypted_interconnect_router = true
   bgp {
@@ -56,7 +56,7 @@ resource "google_compute_router" "router" {
 }
 
 resource "google_compute_network" "network" {
-  name                    = "on-dev-appid-sys-bktest-network"
+  name                    = "on-dev-appid-syst-bktest-network"
   auto_create_subnetworks = false
 }
 
