9b950207eb7198eabc8983c3894337c3098c51d9
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-25 15:17:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,24 @@ resource "google_compute_interconnect_attachment" "on_prem" {
   name                     = "on-dev-appid-sys-bkonprem-icvlan"
   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
   type                     = "PARTNER"
-#  router                   = google_compute_router.foobar.id
+  router                   = google_compute_router.foobar.id
   mtu                      = 1460
 }
 
+resource "google_compute_router" "foobar" {
+  name    = "on-dev-appid-sys-bkonprem-router"
+  network = google_compute_network.foobar.name
+  bgp {
+    asn = 16550
+  }
+}
 
 
+resource "google_compute_router" "router" {
+  name                          = "on-dev-appid-sys-test-router"
+  network                       = google_compute_network.network.name
+  encrypted_interconnect_router = true
+  bgp {
+    asn = 16550
+  }
+}
