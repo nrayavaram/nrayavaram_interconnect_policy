d9a319a5c5b321d5dd0bf5c16a96de718e9fbe25
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-25 15:11:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,52 +10,7 @@ resource "google_compute_interconnect_attachment" "on_prem" {
   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
   type                     = "PARTNER"
   router                   = google_compute_router.foobar.id
-  mtu                      = 1500
+  mtu                      = 1460
 }
 
-resource "google_compute_router" "foobar" {
-  name    = "on-dev-appid-sys-bkonprem-router"
-  network = google_compute_network.foobar.name
-  bgp {
-    asn = 16550
-  }
-}
 
-resource "google_compute_network" "foobar" {
-  name                    = "on-dev-appid-sys-bkonprem-network"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_interconnect_attachment" "ipsec-encrypted-interconnect-attachment" {
-  name                     = "on-dev-appid-sys-bkonprem-attachment"
-  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
-  type                     = "PARTNER"
-  router                   = google_compute_router.router.id
-  encryption               = "IPSEC"
-  ipsec_internal_addresses = [
-    google_compute_address.address.self_link,
-  ]
-}
-
-resource "google_compute_address" "address" {
-  name          = "on-dev-appid-sys-test-address"
-  address_type  = "INTERNAL"
-  purpose       = "IPSEC_INTERCONNECT"
-  address       = "192.168.1.0"
-  prefix_length = 29
-  network       = google_compute_network.network.self_link
-}
-
-resource "google_compute_router" "router" {
-  name                          = "on-dev-appid-sys-test-router"
-  network                       = google_compute_network.network.name
-  encrypted_interconnect_router = true
-  bgp {
-    asn = 16550
-  }
-}
-
-resource "google_compute_network" "network" {
-  name                    = "on-dev-appid-sys-test-network"
-  auto_create_subnetworks = false
-}
