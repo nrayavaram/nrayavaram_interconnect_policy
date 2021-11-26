68c4c2ebe7b85ff6903ce5f7fb90148bde3c1cc6
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-26 17:17:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,26 +17,11 @@ resource "google_compute_interconnect_attachment" "on_prem" {
   mtu                      = 1500
 }
 
-#resource "google_compute_router" "foobar" {
-#  name    = "router"
-#  network = google_compute_network.foobar.name
-#  bgp {
-#    asn = 16550
-#  }
-#}
-
-#resource "google_compute_address" "address" {
-#  name          = "test-address"
-#  address_type  = "INTERNAL"
-#  purpose       = "IPSEC_INTERCONNECT"
-#  address       = "192.168.1.0"
-#  prefix_length = 29
-#  network       = google_compute_network.foobar.self_link
-#}
 
 resource "google_compute_network" "foobar" {
   name                    = "on-dev-appid-syst-bkonprem-network"
   auto_create_subnetworks = false
+  delete_default_routes_on_create = true
   mtu                     = 1500
 }
 
